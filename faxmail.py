3101ca80058b01ca5e2b257542647d059926b386
Kenji Kajita <kajita@field-works.co.jp>
2017-03-06 08:17:06 +0900

    複数のPDFを添付できるようにした。

--- faxmail.py
+++ faxmail.py
@@ -1,45 +1,57 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
+
 SPOOL = '/var/spool/asterisk'
+GS = '/usr/bin/gs'
 
-def pdf2tif(pdf, tif_file):
-    import subprocess
-    command = [
-	'/usr/bin/gs', '-q', '-dNOPAUSE', '-dBATCH',
-	'-sDEVICE=tiffg4', '-sPAPERSIZE=a4',
-	'-sOutputFile='+tif_file, '-'
-    ]
-    p = subprocess.Popen(command, stdin=subprocess.PIPE)
-    p.communicate(pdf)
-
-def fetch_pdf(f):
-    import email
-    message = email.message_from_file(f)
-    for part in message.walk():
-	if part.get_content_type() == 'application/pdf':
-	    return part.get_payload(decode=True)
-
-def create_callfile(trunk, extension, tif_file, call_file):
-    outgoing = '''Channel: SIP/{extension}@{trunk}
+OUTGOING = '''Channel: SIP/{extension}@{trunk}
 WaitTime: 30
 Maxretries: 3
 RetryTime: 300
 Application: SendFax
-Data: {tif_file}'''
-    with open(call_file, 'w') as f:
-	f.write(outgoing.format(trunk=trunk, extension=extension, tif_file=tif_file))
+Data: {data}
+'''
+
+def fetch_pdfs(stream, basename):
+    """メールに添付されているPDFを抽出してファイルとして書き出す。"""
+    import email
+    message = email.message_from_file(stream)
+    for i, part in enumerate(message.walk()):
+        if part.get_content_type() == 'application/pdf':
+            pdf_file = os.path.join(SPOOL, 'fax', basename + str(i) + '.pdf')
+            with open(pdf_file, 'w') as pdffile:
+                pdffile.write(part.get_payload(decode=True))
+            yield pdf_file
+
+def pdf2tif(pdf_files, basename):
+    """複数のPDFファイルを結合して，ひとつのTIFFファイルに変換する。"""
+    import subprocess
+    tif_file = os.path.join(SPOOL, 'fax', basename + '.tif')
+    command = [
+        GS, '-q', '-dNOPAUSE', '-dBATCH',
+        '-sDEVICE=tiffg4', '-sPAPERSIZE=a4',
+        '-sOutputFile='+tif_file] + pdf_files
+    proc = subprocess.Popen(command)
+    proc.communicate()
+    return tif_file
+
+def create_callfile(trunk, extension, tif_file, basename):
+    """callfileを作成する。"""
+    filename = os.path.join(SPOOL, 'outgoing', basename)
+    with open(filename, 'w') as callfile:
+        callfile.write(OUTGOING.format(trunk=trunk, extension=extension, data=tif_file))
+
+def main(trunk, extension):
+    import time
+    basename = str(int(time.time()))
+    pdf_files = fetch_pdfs(sys.stdin, basename)
+    tif_file = pdf2tif(list(pdf_files), basename)
+    create_callfile(trunk, extension, tif_file, basename)
+    for path in pdf_files:
+        os.remove(path)
 
 if __name__ == '__main__':
     import sys
-    import os
-    from datetime import datetime
-    trunk = sys.argv[1]
-    extension = sys.argv[2]
-    basename = datetime.now().strftime("%Y%m%d%H%M%S")
-    tif_file = os.path.join(SPOOL, 'fax', basename + '.tif')
-    call_file = os.path.join(SPOOL, 'outgoing', basename)
-
-    pdf = fetch_pdf(sys.stdin)
-    pdf2tif(pdf, tif_file)
-    create_callfile(trunk, extension, tif_file, call_file)
+    main(sys.argv[1], sys.argv[2])
