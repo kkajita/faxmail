0493a080d8a08db7b1ab3a4dbf4cb23f3f0d2037
Kenji Kajita <kajita@field-works.co.jp>
2017-04-29 14:59:28 +0900

    HTMLメールをFAXとして送信できるようにした。

--- sendfax.py
+++ sendfax.py
@@ -3,17 +3,44 @@
 """
 Email to FAX gateway for Asterisk
 """
-
 import os
+import subprocess
 
 TEMP_DIR = '/tmp'
 TIFF_DIR = '/var/spool/asterisk/fax'
 OUTGOING_DIR = '/var/spool/asterisk/outgoing'
-GS = 'gs'
-CONVERT = 'convert'
-#RESOLUTION = '204x98'
-RESOLUTION = '204x196'
-#RESOLUTION = '204x392'
+
+# 送信対象型式のデフォルト値
+# pdf, tiff, jpeg, png, html,...
+DEFAULT_SUBTYPES = ['pdf', 'html']
+
+def image2pdf_command(from_file, to_file):
+    """
+    ラスタ画像→PDF変換コマンド
+    ラスタ画像の抽出を行わない(添付画像を無視する)場合は，`return None`に変更
+    """
+    return ['convert', from_file, to_file]
+
+def html2pdf_command(from_file, to_file):
+    """
+    HTML→PDF変換コマンド
+    HTML本文を送信しない場合は，`return None`に変更
+    """
+    #return ['xvfb-run', 'wkhtmltopdf', '--encoding', 'utf-8', '--dpi', '360', from_file, to_file]
+    #return ['xvfb-run', 'wkhtmltopdf', '--dpi', '360', from_file, to_file]
+    return ['wkhtmltopdf', '--dpi', '400', from_file, to_file]
+
+def pdfs2tiff_command(pdf_files, tiff_file):
+    """
+    複数PDF→TIFF変換コマンド
+    - 標準 '-r204x98'
+    - ファイン '-r204x196'
+    - スーパーファイン '-r204x392'
+    """
+    return [
+        'gs', '-q', '-dNOPAUSE', '-dBATCH',
+        '-sDEVICE=tiffg4', '-sPAPERSIZE=a4', '-dFIXEDMEDIA', '-dPDFFitPage', '-r204x196',
+        '-sOutputFile='+tiff_file] + pdf_files
 
 OUTGOING_MESSAGE = '''Channel: SIP/{faxnumber}@{trunk}
 WaitTime: 30
@@ -29,54 +56,63 @@ Set: REPLYTO={replyto}
 Set: SUBJECT={subject}
 '''
 
-def convert(from_file, to_file):
-    "ImageMagickを使って画像形式を変換"
-    import subprocess
-    proc = subprocess.Popen([CONVERT, from_file, to_file])
-    proc.communicate()
-    return to_file
+def convert(command, from_file, to_file):
+    "画像形式変換コマンドを実行"
+    args = command(from_file, to_file)
+    if args:
+        proc = subprocess.Popen(args)
+        proc.communicate()
+        return to_file
+    return None
 
-def extract_pdfs(message, basename):
-    "添付されている画像ファイルをいったんPDF形式に変換"
+def extract_pdfs(message, basename, targets):
+    "送信対象のMIMEパートを抽出してPDF形式に変換"
     def temp_file(i, ext):
         "一時ファイル名を生成"
-        return os.path.join(TEMP_DIR, basename + str(i) + ext)
-    def extract(part, path):
+        return os.path.join(TEMP_DIR, basename + str(i) + '.' + ext)
+    def writefile(utf8str, path):
         "MIMEパートから抽出したデータをファイル化"
-        with open(path, 'w') as f:
-            f.write(part.get_payload(decode=True))
+        with open(path, 'wb') as f:
+            f.write(utf8str)
         return path
     for i, part in enumerate(message.walk()):
-        pdf_file = temp_file(i, '.pdf')
-        if part.get_content_type() == 'application/pdf':
-            yield extract(part, pdf_file)
-        elif part.get_content_type() == 'image/jpeg':
-            jpeg_file = extract(part, temp_file(i, '.jpeg'))
-            yield convert(jpeg_file, pdf_file)
-        elif part.get_content_type() == 'image/png':
-            png_file = extract(part, temp_file(i, '.png'))
-            yield convert(png_file, pdf_file)
+        mtype, subtype = part.get_content_type().split('/')
+        if subtype not in targets:
+            continue
+        data = part.get_payload(decode=True)
+        if mtype == 'applicatione' and subtype == 'pdf':
+            yield writefile(data, temp_file(i, 'pdf'))
+        elif mtype == 'image':
+            image_file = writefile(data, temp_file(i, subtype))
+            yield convert(image2pdf_command, image_file, temp_file(i, 'pdf'))
+        elif mtype == 'text' and subtype == 'html':
+            from bs4 import BeautifulSoup
+            root = BeautifulSoup(data, "html.parser")
+            meta = BeautifulSoup('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">', "html.parser")
+            root.head.append(meta)
+            html_file = writefile(str(root), temp_file(i, subtype))
+            yield convert(html2pdf_command, html_file, temp_file(i, 'pdf'))
 
 def pdfs2tif(pdf_files, basename):
     "複数のPDFファイルをひとつのTIFFファイルに変換"
-    import subprocess
     tif_file = os.path.join(TIFF_DIR, basename + '.tif')
-    command = [
-        GS, '-q', '-dNOPAUSE', '-dBATCH',
-        '-sDEVICE=tiffg3', '-sPAPERSIZE=a4', '-dFIXEDMEDIA', '-dPDFFitPage', '-r'+RESOLUTION,
-        '-sOutputFile='+tif_file] + pdf_files
-    proc = subprocess.Popen(command)
-    proc.communicate()
-    return tif_file
+    return convert(pdfs2tiff_command, pdf_files, tif_file)
 
 def create_callfile(basename, **params):
     """
     Asteriskに受け渡すcallfileを作成する。
-    本来は，他のディレクトリで作成してからmoveすべき。
+    本来は，他のディレクトリで作成してからmoveしたほうが安全
     """
     call_file = os.path.join(OUTGOING_DIR, basename + '.call')
     with open(call_file, 'w') as f:
         f.write(OUTGOING_MESSAGE.format(**params))
+
+def extract_subtypes(subject):
+    "Subject文字列からオプションを抽出"
+    import re
+    plus = set(re.findall(r'\+(\w+)', subject))
+    minus = set(re.findall(r'\-(\w+)', subject))
+    return set(DEFAULT_SUBTYPES).union(plus).difference(minus)
 
 def sendfax(message, context, trunk, faxnumber):
     "メールメッセージから画像を抽出して，FAX送信するようAsteriskに指示する。"
@@ -84,7 +120,8 @@ def sendfax(message, context, trunk, faxnumber):
     basename = str(int(time.time()))
     replyto = message.get('Reply-To', message['From'])
     subject = message['Subject'] if message['Subject'] else 'Send Fax to ' + faxnumber
-    pdf_files = list(extract_pdfs(message, basename))
+    subtypes = extract_subtypes(subject)
+    pdf_files = [f for f in extract_pdfs(message, basename, subtypes) if f]
     tif_file = pdfs2tif(pdf_files, basename) if pdf_files else '<<EMPTY>>'
     create_callfile(basename, context=context, trunk=trunk, faxnumber=faxnumber,
                     faxfile=tif_file, replyto=replyto, subject=subject)
