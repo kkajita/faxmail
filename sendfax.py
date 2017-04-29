05cc4c781b7553683afdbfd00c1d7f3ad8bc6d00
Kenji Kajita <kajita@field-works.co.jp>
2017-04-29 20:26:49 +0900

    text/plainも選択できるようにした。

--- sendfax.py
+++ sendfax.py
@@ -16,12 +16,17 @@ RESOLUTIONS = {
 }
 
 # 送信対象型式のデフォルト値
-# pdf, tiff, jpeg, png, html,...
-DEFAULT_SUBTYPES = ['pdf', 'html']
+# pdf, tiff, jpeg, png, html, plain
+DEFAULT_SUBTYPES = ['pdf', 'tiff', 'jpeg', 'png']
 
 def image2pdf_command(from_file, to_file):
     "ラスタ画像→PDF変換コマンド"
     return ['convert', from_file, to_file]
+
+def plain2pdf_command(encoding, from_file, to_file):
+    "テキスト→PDF変換コマンド"
+    return ['/usr/local/bin/wkhtmltopdf', '--disable-smart-shrinking', '--dpi', '360',
+            '--encoding', encoding, from_file, to_file]
 
 def html2pdf_command(from_file, to_file):
     "HTML→PDF変換コマンド"
@@ -68,16 +73,21 @@ def extract_pdfs(message, basename, targets):
             f.write(utf8str)
         return path
     for i, part in enumerate(message.walk()):
-        mtype, subtype = part.get_content_type().split('/')
+        maintype, subtype = part.get_content_maintype(), part.get_content_subtype()
         if subtype not in targets:
             continue
         data = part.get_payload(decode=True)
-        if mtype == 'applicatione' and subtype == 'pdf':
+        if maintype == 'applicatione' and subtype == 'pdf':
             yield writefile(data, temp_file(i, 'pdf'))
-        elif mtype == 'image':
+        elif maintype == 'image':
             image_file = writefile(data, temp_file(i, subtype))
             yield convert(image2pdf_command, image_file, temp_file(i, 'pdf'))
-        elif mtype == 'text' and subtype == 'html':
+        elif maintype == 'text' and subtype == 'plain':
+            import functools
+            command = functools.partial(plain2pdf_command, part.get_content_charset())
+            plain_file = writefile(data, temp_file(i, 'txt'))
+            yield convert(command, plain_file, temp_file(i, 'txt'))
+        elif maintype == 'text' and subtype == 'html':
             from bs4 import BeautifulSoup
             root = BeautifulSoup(data, "html.parser")
             meta = BeautifulSoup('<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">', "html.parser")
@@ -88,8 +98,8 @@ def extract_pdfs(message, basename, targets):
 def pdfs2fax(quality, pdf_files, basename):
     "複数のPDFファイルをひとつのTIFFファイルに変換"
     import functools
-    fax_file = os.path.join(TIFF_DIR, basename + '.tiff')
     command = functools.partial(raster_command, quality)
+    fax_file = os.path.join(TIFF_DIR, basename + '.tiff')
     return convert(command, pdf_files, fax_file)
 
 def create_callfile(basename, **params):
@@ -113,13 +123,13 @@ def get_quality(options, quality):
     setting = options.intersection(set(RESOLUTIONS.keys()))
     return setting.pop() if setting else quality
 
-def sendfax(message, context, trunk, faxnumber, quality):
+def sendfax(message, context, trunk, faxnumber, types, quality):
     "メールメッセージから画像を抽出して，FAX送信するようAsteriskに指示する。"
     import time
     basename = str(int(time.time()))
     replyto = message.get('Reply-To', message['From'])
     subject = message['Subject'] if message['Subject'] else 'Send Fax to ' + faxnumber
-    options = extract_options(subject)
+    options = extract_options(subject).union(set(types))
     pdf_files = [f for f in extract_pdfs(message, basename, options) if f]
     fax_file = pdfs2fax(get_quality(options, quality), pdf_files, basename) if pdf_files else '<<EMPTY>>'
     create_callfile(basename, context=context, trunk=trunk, faxnumber=faxnumber,
@@ -131,13 +141,14 @@ def main():
     import argparse
     import email
     par = argparse.ArgumentParser(description=__doc__)
-    par.add_argument('-q', '--quality', default='fine', choices=RESOLUTIONS.keys(), help='fax resolution')
     par.add_argument('context', help='context name')
     par.add_argument('trunk', help='SIP trunk')
     par.add_argument('number', help='FAX number')
+    par.add_argument('-q', '--quality', default='fine', choices=RESOLUTIONS.keys(), help='fax resolution')
+    par.add_argument('-t', '--types', action='append', help='content types')
     args = par.parse_args()
     message = email.message_from_file(sys.stdin)
-    sendfax(message, args.context, args.trunk, args.number, args.quality)
+    sendfax(message, args.context, args.trunk, args.number, args.types, args.quality)
 
 if __name__ == '__main__':
     main()
